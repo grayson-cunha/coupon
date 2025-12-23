5e5e030bf34434b5adf8dffe29dbcb2bd0e63d6d
Grayson Cunha <graysonsilva93@gmail.com>
2025-12-23 02:13:20 -0300

    chore: configure docker and swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-validation:4.0.1")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.0")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
---
+++ dockerfile
@@ -0,0 +1,15 @@
+FROM gradle:9.2.1-jdk21-alpine AS build
+WORKDIR /home/app
+
+COPY . .
+
+RUN gradle clean bootJar --no-daemon
+
+FROM amazoncorretto:21-alpine
+WORKDIR /app
+
+COPY --from=build /home/app/build/libs/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]%
